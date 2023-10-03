5d1d02ac1f16294597c0094f0b9da8134ae1f141
Arshdeep-Singh-01 <2020csb1074@iitrpr.ac.in>
2023-10-03 00:11:44 -0400

    utilities added

---
+++ utils/Plot/plotInputData.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+Fs = 25 # frame size
+
+def plot_activity(activity, data):
+    fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize=(15, 7), sharex=True)
+    plot_axis(ax0, data['time'], data['x'], 'X-Axis')
+    plot_axis(ax1, data['time'], data['y'], 'Y-Axis')
+    plot_axis(ax2, data['time'], data['z'], 'Z-Axis')
+    plt.subplots_adjust(hspace=0.2)
+    fig.suptitle(activity)
+    plt.subplots_adjust(top=0.90)
+    plt.show()
+
+def plot_axis(ax, x, y, title):
+    ax.plot(x, y, 'g')
+    ax.set_title(title)
+    ax.xaxis.set_visible(False)
+    ax.set_ylim([min(y) - np.std(y), max(y) + np.std(y)])
+    ax.set_xlim([min(x), max(x)])
+    ax.grid(True)
+
+
+def plotData(data):
+    activities = activities = data['activity'].value_counts().index
+    for activity in activities:
+        data_for_plot = data[(data['activity'] == activity)][:Fs*10]
+        plot_activity(activity, data_for_plot)
